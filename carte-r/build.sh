97c221011936dcfe1bfd207b0c948fd2e58ff6b9
Rajiv Narayan <perturbomics@zohomail.com>
2024-10-27 17:50:34 -0400

    Moved Docker file to assets for quay

--- carte-r/build.sh
+++ carte-r/build.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 self_path=$(dirname $0)
 cd ${self_path}/assets
-docker build -f ../Dockerfile --no-cache --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg BUILD_VERSION="latest" -t carte-r:latest .
+docker build -f ./Dockerfile --no-cache --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg BUILD_VERSION="latest" -t carte-r:latest .
